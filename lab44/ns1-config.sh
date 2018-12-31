53edc39ef5ea2d4e4ab52aa46c9cafe1c4dc2929
PiotrAF <piotraf@users.noreply.github.com>
2018-12-31 16:32:47 +0100

    Create ns1-config.sh

---
+++ lab44/ns1-config.sh
@@ -0,0 +1,9 @@
+## assumption - the system was created with ns1.ks and the network interfaces names are:
+## connection to the internet dynamic IP - interface after innstalation is named "System enp0s3"
+## connection to the intranet (LAB44) static IP 192.168.44.254 - interface after innstalation is named "System enp0s8"
+# renaming the network interfaces to get rid of the "System" and "enp0sX" naming
+nmcli connection modify "System enp0s3" connection.interface-name "external"
+nmcli connection modify "System enp0s8" connection.interface-name "internal"
+## create DHCP server for the LAB44
+
+
