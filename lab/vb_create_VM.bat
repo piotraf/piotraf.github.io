3a2b6d9a7f806d34cc1526453c68869a997bf0f1
PiotrAF <piotraf@users.noreply.github.com>
2019-01-02 15:25:44 +0100

    Update vb_create_VM.bat

--- lab/vb_create_VM.bat
+++ lab/vb_create_VM.bat
@@ -14,3 +14,36 @@ VBoxManage storagectl %VMNAME% --name "IDE Controller" --add ide
 ==
 VBoxManage unattended install "test02" --iso="D:\iso"/CentOS-7-x86_64-Minimal-1810.iso --extra-install-kernel-parameters="inst.ks=http://piotraf.github.io/lab/centos7mini.ks"
 
+VBoxManage.exe: info: Preparing unattended installation of RedHat_64 in machine 'test02' (ef191167-f5ba-4312-b6bd-5d05204a0248).
+VBoxManage.exe: info: Using values:
+                           isoPath = D:\iso\CentOS-7-x86_64-Minimal-1810.iso
+                              user = vboxuser
+                          password = changeme
+                      fullUserName =
+                        productKey =
+                  additionsIsoPath = C:\Program Files\Oracle\VirtualBox/VBoxGuestAdditions.iso
+             installGuestAdditions = false
+              validationKitIsoPath =
+            installTestExecService = false
+                            locale = en_US
+                           country = PL
+                          timeZone = Central Europe Standard Time
+                             proxy =
+                          hostname = test02.myguest.virtualbox.org
+       packageSelectionAdjustments =
+                 auxiliaryBasePath = D:\VirtualBoxVMs\SYSTEM\test02\Unattended-ef191167-f5ba-4312-b6bd-5d05204a0248-
+                        imageIndex = 1
+                scriptTemplatePath = C:\Program Files\Oracle\VirtualBox\UnattendedTemplates\redhat67_ks.cfg
+     postInstallScriptTemplatePath = C:\Program Files\Oracle\VirtualBox\UnattendedTemplates\redhat_postinstall.sh
+                postInstallCommand =
+      extraInstallKernelParameters = inst.ks=http://piotraf.github.io/lab/centos7mini.ks
+                          language = en-US
+                  detectedOSTypeId = RedHat_64
+                 detectedOSVersion = 7
+                  detectedOSFlavor =
+               detectedOSLanguages = en-US
+                   detectedOSHints =
+VBoxManage.exe: info: VM 'test02' (ef191167-f5ba-4312-b6bd-5d05204a0248) is ready to be started (e.g. VBoxManage startvm).
+VBoxManage startvm "test02"
+Waiting for VM "test02" to power on...
+VM "test02" has been successfully started.
