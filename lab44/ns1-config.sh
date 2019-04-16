1f33add1fb064aa145778f66caeb9aa6aff6b663
PiotrAF <piotraf@users.noreply.github.com>
2019-04-16 18:43:22 +0200

    added section "create DNS"
    added new section create unbound DNS

--- lab44/ns1-config.sh
+++ lab44/ns1-config.sh
@@ -12,13 +12,46 @@
 # assumption - the system was created with ns1.ks and the network interfaces names are:
 # connection to the internet dynamic IP - interface after innstalation is named "System enp0s3"
 # connection to the intranet (LAB44) static IP 192.168.44.254 - interface after innstalation is named "System enp0s8"
-# renaming the network interfaces to get rid of the "System" and "enp0sX" naming
-nmcli connection down "System enp0s3"
-nmcli connection down "System enp0s8"
-nmcli connection modify "System enp0s3" connection.interface-name "external"
-nmcli connection modify "System enp0s8" connection.interface-name "internal"
-nmcli connection down "external"
-nmcli connection down "internal"
+# create Name Server 
+yum install unbound -y 
+cp /etc/unbound/unbound.conf{,.orig}
+sed -i 's/# interface: 0.0.0.0$/interface: 0.0.0.0/g' /etc/unbound/unbound.conf
+sed -i 's/\# access-control: 127.0.0.0\/8 allow/access-control: 192.168.0.0\/16 allow/g' /etc/unbound/unbound.conf
+mv /etc/unbound/conf.d/example.com.conf{,.stub-zone}
+cat >> /etc/unbound/conf.d/example.com.conf << "EOF"
+server:
+local-zone: "example.com." static
+local-data: "ns1.example.com.  IN A 192.168.44.254"
+local-data-ptr: "192.168.44.254  ns1.example.com"
+local-data-ptr: "192.168.44.1  vbox.example.com"
+local-data: "vbox.example.com.  IN A 192.168.44.1"
+###############
+EOF
+cat >> /etc/unbound/conf.d/forward-zone.conf << "EOF"
+##########
+forward-zone:
+      name: "."
+      forward-addr: 8.8.4.4        # Google
+      forward-addr: 8.8.8.8        # Google
+      forward-addr: 37.235.1.174   # FreeDNS
+      forward-addr: 37.235.1.177   # FreeDNS
+      forward-addr: 50.116.23.211  # OpenNIC
+      forward-addr: 64.6.64.6      # Verisign
+      forward-addr: 64.6.65.6      # Verisign
+      forward-addr: 74.82.42.42    # Hurricane Electric
+      forward-addr: 84.200.69.80   # DNS Watch
+      forward-addr: 84.200.70.40   # DNS Watch
+      forward-addr: 91.239.100.100 # censurfridns.dk
+      forward-addr: 109.69.8.51    # puntCAT
+      forward-addr: 208.67.222.220 # OpenDNS
+      forward-addr: 208.67.222.222 # OpenDNS
+      forward-addr: 216.146.35.35  # Dyn Public
+      forward-addr: 216.146.36.36  # Dyn Public
+############
+EOF
+systemctl enable --now unbound
+firewall-cmd --zone=internal --add-service=dns --permanent
+firewall-cmd --reload
 ## create DHCP server for the LAB44
 
 
