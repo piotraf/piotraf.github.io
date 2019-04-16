8035abcd44ea109c0e60e901992415b5842fb238
PiotrAF <piotraf@users.noreply.github.com>
2019-04-16 20:20:20 +0200

    initial steps to prepare for oracle database

---
+++ lab44/oracle_prep.sh
@@ -0,0 +1,10 @@
+##############################################################
+#   Preparing http://piotraf.github.io/lab44/c7.ks           #
+#   template installation                                    #
+#   for Oracle database installation                         #
+##############################################################
+login via root
+ip a to show what IP assigned
+yum update -y&&reboot
+login as admin via putty
+sudo su -
